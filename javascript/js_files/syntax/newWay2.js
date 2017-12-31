b84d7db700f156b98d5896746e6b85fcd34bd5a0
dikshajoshi44 <dikshajoshi131@gmail.com>
2017-12-31 19:19:01 +0530

    js files new way of writing js template literals

---
+++ javascript/js_files/syntax/newWay2.js
@@ -0,0 +1,3 @@
+const myName = '[NAME]';
+const greeting = `Hello, my name is ${myName}`;
+console.log(greeting);
---
+++ javascript/js_files/syntax/newWayString Concat.js
@@ -0,0 +1,12 @@
+const student = {
+  name: 'Richard Kalehoff',
+  guardian: 'Mr. Kalehoff'
+};
+
+const teacher = {
+  name: 'Mrs. Wilson',
+  room: 'N231'
+}
+
+let message = student.name + ' please see ' + teacher.name + ' in ' + teacher.room + ' to pick up your report card.';
+let message = `${student.name} please see ${teacher.name} in ${teacher.room} to pick up your report card.`;
